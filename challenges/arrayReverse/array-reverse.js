6fb240fdbdab6f5fa36750b2149a56c81d60d4be
yasminadaileh1 <yas.adaileh1@gmail.com>
2020-05-17 20:17:51 +0300

    array-reverse

---
+++ challenges/arrayReverse/array-reverse.js
@@ -0,0 +1,10 @@
+function reverseArray(arr) {
+    let reversed = [];
+    for (var i = array.length; i > 0; i--) {
+        reversed.push(array.pop());
+        // console.log('i = ', i)
+    }
+
+    return reversed;
+
+}
