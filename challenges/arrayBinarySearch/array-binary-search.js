3355f363a6977b51beeea755219f3afb9bc40e06
yasminadaileh1 <yas.adaileh1@gmail.com>
2020-05-19 19:22:53 +0300

    done

---
+++ challenges/arrayBinarySearch/array-binary-search.js
@@ -0,0 +1,15 @@
+"use strict";
+
+function binarySearch(arr, num) {
+  for (let i = 0; i < arr.length; i++) {
+    if (num === arr[i]) {
+      return i;
+    }
+  }
+  let inCase = -1;
+  if (inCase) {
+    return inCase;
+  }
+}
+
+module.exports = binarySearch;
