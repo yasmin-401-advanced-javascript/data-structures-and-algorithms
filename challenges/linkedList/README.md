99f5627de715940a13a05f01f9e67e1194d78bc9
yasminadaileh1 <yas.adaileh1@gmail.com>
2020-06-01 02:10:32 +0300

    code challenge 6

--- challenges/linkedList/README.md
+++ challenges/linkedList/README.md
@@ -10,6 +10,34 @@ the most challenging part was with the `toString method` and still because I cou
 
 class constructor & while loop
 
+### the Big O is Big O Of(n)
+
+#### `insert(value)`
+- time: Big O Of(1)
+- space: Big O Of(3)
+
+#### `append(value)`
+- time: Big O Of(n)
+- space: Big O Of(3)
+
+#### `insertBefore(value,newValue)`
+- time: Big O Of(n)
+- space: Big O Of(5)
+
+#### `insertAfter(value,newValue)`
+- time: Big O Of(n)
+- space: Big O Of(5)
+
+#### `includes(value)`
+
+- time: Big O Of(n)
+- space: Big O Of(2)
+
+#### `toString()`
+- time: Big O Of(n)
+- space: Big O Of(2)
+
+
 ## Solution
 
 ![whiteBoard]()
--- challenges/linkedList/linked-list.js
+++ challenges/linkedList/linked-list.js
@@ -5,6 +5,7 @@ class LinkedList {
   constructor() {
     this.head = null;
   }
+  ////////////////////////////////
   insert(value) {
     const node = new Node(value);
     if (!this.head) {
@@ -16,6 +17,22 @@ class LinkedList {
     node.next = nextHead;
     return this;
   }
+  /////////////////////////
+  append(value){
+    const node = new Node(value);
+    let nextHead  = this.head;
+    if (!this.head) {
+      this.head = node;
+      return this;
+    }
+    while(nextHead.next){
+      nextHead = nextHead.next;
+    }
+    nextHead.next = node;
+    return this;
+  }
+
+  /////////////////////////////////////
   include(values){
     let nextHead = this.head;
     if(nextHead.value === values){
@@ -30,7 +47,7 @@ class LinkedList {
     }
     return false;
   }
-
+  ////////////////////////
   toString(){
     let nextHead = this.head;
     let str = `{ ${nextHead.value} } -> `;
@@ -40,6 +57,55 @@ class LinkedList {
     }
     str += 'NULL';
     return str;
+  }
+  //////////////////////////////
+  insertBefore(value, newVal){
+    if(value && newVal){
+      let node = new Node(newVal);
+      if(this.head.value === value){
+        let nextHead = this.head;
+        this.head= node ;
+        this.head.next = nextHead;
+        return this;
+      }
+      let nextHead = this.head;
+      while(nextHead.next){
+        var previousValue = nextHead.next ;
+
+        if(nextHead.next.value === value){
+          nextHead.next = node ;
+        }
+        nextHead = nextHead.next ;
+      }
+      nextHead.next = previousValue;
+      return this ;
+    }else{
+      return 'please enter the current value and the new value';
+    }
+  }
+  insertAfter(value, newVal){
+    if( value && newVal){
+      let node = new Node(newVal);
+      if(this.head.value === value){
+        let stableHead = this.head.next;
+        this.head.next = node;
+        this.head.next.next = stableHead;
+        return this;
+      }
+      let nextHead = this.head;
+      while(nextHead.next){
+        nextHead = nextHead.next ;
+        if(nextHead.value === value){
+          let stableHead = nextHead.next;
+          nextHead.next = node ;
+          nextHead.next.next = stableHead;
+          return this;
+        }
+      }
+    }else{
+      return 'please enter the current value and the new value';
+    }
+
   }
 }
 module.exports = LinkedList;
