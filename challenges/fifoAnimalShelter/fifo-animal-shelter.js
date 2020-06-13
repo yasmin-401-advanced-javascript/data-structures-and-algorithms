735edffbbe96e46e62e4b29aea1ae40cfaf080d7
yasminadaileh1 <yas.adaileh1@gmail.com>
2020-06-13 23:46:54 +0300

    challenge 12

---
+++ challenges/fifoAnimalShelter/fifo-animal-shelter.js
@@ -0,0 +1,54 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class AnimalShelter{
+  constructor(){
+    this.storge = [];
+    this.front = null;
+  }
+
+  enqueue(animal) {
+    if(animal === 'cat' || animal === 'dog'){
+      let node = new Node(animal);
+      if (this.front === null) {
+        this.front = node;
+      } else {
+        node.next = this.front;
+        this.front = node;
+      }
+    }else{
+      return 'only dogs and cats';
+    }
+
+  }
+  dequeue(animal) {
+    if(this.front){
+      if (animal === 'cat' || animal === 'dog') {
+        if(this.front.next === null){
+          let poped = this.front;
+          this.front = null;
+          return poped
+        }
+        let current = this.front;
+        let parent = null;
+        while(current.next){
+          parent = current;
+          current= current.next
+        }
+        parent.next = null
+        return current;
+      }else {
+        return null
+      }
+    }else{
+      return 'shelter is empty'
+    }
+
+  }
+}
+
+module.exports = AnimalShelter;
